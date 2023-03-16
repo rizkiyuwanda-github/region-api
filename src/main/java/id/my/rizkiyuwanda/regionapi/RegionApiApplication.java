0248ab3f12353094b066744474921e8d39fc4576
Rizki Yuwanda <yuwanda.rizki@gmail.com>
2023-03-16 21:14:58 +0800

    init project

---
+++ src/main/java/id/my/rizkiyuwanda/regionapi/RegionApiApplication.java
@@ -0,0 +1,13 @@
+package id.my.rizkiyuwanda.regionapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegionApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegionApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/my/rizkiyuwanda/regionapi/RegionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.rizkiyuwanda.regionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegionApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
